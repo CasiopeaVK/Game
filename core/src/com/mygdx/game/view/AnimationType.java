b8ffbae5a3afdcc143150c951cafd095d39f34b5
Дмитро Жорник <dmytro.zhornyk@ukma.edu.ua>
2020-05-23 12:50:15 +0300

    Improved animationType, created hero animation pack

--- core/src/com/mygdx/game/view/AnimationType.java
+++ core/src/com/mygdx/game/view/AnimationType.java
@@ -1,5 +1,20 @@
 package com.mygdx.game.view;
 
 public enum  AnimationType {
-    HERO();
+    HERO_MOVE_UP("hero/character_movement.atlas", "hero", 0.05f, 2),
+    HERO_MOVE_DOWN("hero/character_movement.atlas", "hero", 0.05f, 0),
+    HERO_MOVE_LEFT("hero/character_movement.atlas", "hero", 0.05f, 3),
+    HERO_MOVE_RIGHT("hero/character_movement.atlas", "hero", 0.05f, 1);
+
+    private final String atlasPath;
+    private final String atlasKey;
+    private final float frameTime;
+    private final int index;
+
+    AnimationType(final String atlasPath, String atlasKey, float frameTime, int index) {
+        this.atlasPath = atlasPath;
+        this.atlasKey = atlasKey;
+        this.frameTime = frameTime;
+        this.index = index;
+    }
 }
