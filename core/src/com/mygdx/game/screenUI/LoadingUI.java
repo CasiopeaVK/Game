ce272e31267c11dfa5b55be43291e87ea08c2880
Stanislav Bohuta <stasboguta31@gmail.com>
2020-05-27 11:06:16 +0300

    Add some constant into InventoryCell and LoadingUI

--- core/src/com/mygdx/game/screenUI/LoadingUI.java
+++ core/src/com/mygdx/game/screenUI/LoadingUI.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.mygdx.game.Constants;
 
 public class LoadingUI extends Table {
+    private static final String PROGRESS_BAR_STYLE = "default-horizontal";
+    private static final String LOADING_TEXT_STYLE = "title";
+
     ProgressBar progressBar;
     Label loading;
 
@@ -13,8 +16,8 @@ public class LoadingUI extends Table {
         setSize(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
         setPosition(Gdx.graphics.getWidth()/2-getWidth()/2,0);
 
-        progressBar = new ProgressBar(0,1,0.1f,false,Constants.APP_SKIN,"default-horizontal");
-        loading = new Label("Loading", Constants.APP_SKIN, "title");
+        progressBar = new ProgressBar(0,1,0.1f,false,Constants.APP_SKIN,PROGRESS_BAR_STYLE);
+        loading = new Label("Loading", Constants.APP_SKIN, LOADING_TEXT_STYLE);
 
         this.add(loading).bottom().row();
         this.add(progressBar).expand().fill().bottom();
