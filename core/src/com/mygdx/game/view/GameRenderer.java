6e48a9d0188154e0d95a9682caaaa67d3b91b892
Дмитро Жорник <dmytro.zhornyk@ukma.edu.ua>
2020-06-03 13:27:12 +0300

    Added objects creation

--- core/src/com/mygdx/game/view/GameRenderer.java
+++ core/src/com/mygdx/game/view/GameRenderer.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Disposable;
