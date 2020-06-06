15d67827482e6dfc1adf71ce5bcee6596c3bfd18
BogdanVoylov <voylovbogdan@gmail.com>
2020-06-06 07:10:19 +0300

    changed entityList to concurrently safe ArrayList

--- core/src/com/mygdx/game/stage/SmartStage.java
+++ core/src/com/mygdx/game/stage/SmartStage.java
@@ -9,36 +9,48 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SmartStage extends Stage {
-    private List<Entity> entityList = new ArrayList<>();
+    private List<Entity> entityList = new CopyOnWriteArrayList<>();
     @Setter
     private GameUI gameUI;
-    @Setter @Getter
+    @Setter
+    @Getter
     private Player player;
 
-    public void update(){
+    public void update() {
         gameUI.updateTime();
         player.setCurrentCell();
         act();
-        for(Entity entity:entityList){
-           entity.update();
+        for (Entity entity : entityList) {
+            entity.update();
         }
         draw();
         gameUI.renderSelectedItem(this);
     }
-    public void addEntity(Entity entity){
+
+    public void addEntity(Entity entity) {
         addActor(entity);
         entityList.add(entity);
     }
-    public boolean addItem(Item item){
+
+
+    public void remove(Entity entity) {
+        entityList.remove(entity);
+    }
+
+    public boolean addItem(Item item) {
         return player.addItem(item);
     }
-    public int getCurrentQuestIndex(){
+
+    public int getCurrentQuestIndex() {
         return gameUI.getQuestTable().getQuestLine().getCurrentQuestIndex();
     }
-    public void incrementCurrentQuestIndex(){
+
+    public void incrementCurrentQuestIndex() {
         gameUI.getQuestTable().getQuestLine().incrementQuest();
     }
 
